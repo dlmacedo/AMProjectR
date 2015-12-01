05332b84c2fe131c3cdb9071483df9eb7914ab56
dlmacedo <dlm@cin.ufpe.br>
2015-12-01 16:32:28 -0300

    Few changes...

--- Main.R
+++ Main.R
@@ -75,7 +75,7 @@ for(i in 1:4){
 
 print(globalResults)
 #pdf("GlobalResults.pdf")
-boxplot(globalResults, main="BLOXPLOT FOR CLASSIFIERS")
+boxplot(globalResults, main="BOXPLOT FOR CLASSIFIERS")
 #dev.off()
 write.table(globalResults, "GlobalResults.txt", sep="\t")
 
